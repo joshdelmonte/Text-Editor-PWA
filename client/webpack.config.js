79df3d027af6947c5742f5611780ba81cfd16012
joshdelmonte <joshdelmonte@gmail.com>
2023-01-30 14:14:52 -0700

    Modified webpack.config, and dabase js files logic for db

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,6 +7,7 @@ const { webpack } = require('webpack');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 
+
 // TODO: Add CSS loaders and babel to webpack.
 
 
@@ -18,9 +19,6 @@ module.exports = () => {
       install: './src/js/install.js'
     },
     output: {
-      //Is name the correct thing we should be putting in this filename?
-      //I think it is, but I'm not sure.
-      //I think it's the name of the entry point, which is what we want.
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
@@ -39,8 +37,8 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'PWA Manifest',
-        short_name: 'PWAM',
+        name: 'JATE Manifest',
+        short_name: 'JATE',
         description: 'PWA for everyone!',
         background_color: '#263cb3',
         theme_color: '#263cb3',
