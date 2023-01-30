c0be75ac7aa1192ee73a7a838f3ce7b2c861132e
joshdelmonte <joshdelmonte@gmail.com>
2023-01-30 13:46:31 -0700

    Modifying the last lines of the src-sw.js to appropriately implement caching

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,14 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute( ({ request }) => request.destination === 'image', new CacheFirst({
+  cacheName: 'image-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 500],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+        }),
+        ],
+        }));
